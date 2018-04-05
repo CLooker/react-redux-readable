7ddcbb645f8f8ca0be99b35a9088893b7873ffaf
CLooker <chad.looker@gmail.com>
2018-04-04 20:46:04 -0400

    styling

--- src/components/CreatePostForm.js
+++ src/components/CreatePostForm.js
@@ -11,21 +11,23 @@ const CreatePostForm = ({
   category,
   handleCategoryChange
 }) => (
-  <form className="create-post" onSubmit={handleSubmit}>
-    <label>Title:</label>
-    <input type="text" value={title} onChange={handleTitleChange} />
-    <label>Author:</label>
-    <input type="text" value={author} onChange={handleAuthorChange} />
-    <label>Post Body:</label>
-    <textarea value={body} onChange={handleBodyChange} />
-    <select value={category} onChange={handleCategoryChange}>
-      <option disabled>Select Post Category...</option>
-      <option value="react">React</option>
-      <option value="redux">Redux</option>
-      <option value="udacity">Udacity</option>
-    </select>
-    <button>Submit</button>
-  </form>
+  <div className="create-post-form-container">
+    <form className="create-post" onSubmit={handleSubmit}>
+      <label className="create-post-label-title">Title:</label>
+      <input type="text" value={title} onChange={handleTitleChange} />
+      <label>Author:</label>
+      <input type="text" value={author} onChange={handleAuthorChange} />
+      <label>Post Body:</label>
+      <textarea value={body} onChange={handleBodyChange} />
+      <select value={category} onChange={handleCategoryChange}>
+        <option disabled>Select Post Category...</option>
+        <option value="react">React</option>
+        <option value="redux">Redux</option>
+        <option value="udacity">Udacity</option>
+      </select>
+      <button>Submit</button>
+    </form>
+  </div>
 );
 
 export default CreatePostForm;
