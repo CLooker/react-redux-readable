9e3c3e396b43bbe13a06d5dc3496b0ce24a51367
CLooker <chad.looker@gmail.com>
2018-07-05 18:50:00 -0400

    update resume link

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,7 @@ const Resume = () => (
   <a
     className="resume"
     title="Résumé"
-    href="https://docs.google.com/document/d/1eBIesYRG47j_OEn6b5SwYIdi3vBiFHq_vMs5YzQXb2c/edit?usp=sharing"
+    href="https://docs.google.com/document/d/161S5NQXgqMKpjowmmNkyHYupuVWDJ3KneGsIMjDwpBQ/edit?usp=sharing"
     target="_blank"
     rel="noopener noreferrer"
   >
