ff0b77c9df7b3502a797bb8058df260da1bded5d
CLooker <chad.looker@gmail.com>
2018-12-08 16:42:41 -0500

    use js to create all the footer child elements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,44 @@
 import React from 'react';
-import GitHub from './GitHub';
-import LinkedIn from './LinkedIn';
-import Resume from './Resume';
-import Portfolio from './Portfolio';
+
+const footerItems = {
+  LinkedIn: {
+    href: 'https://www.linkedin.com/in/chad-looker/',
+    imgExtension: 'png'
+  },
+  GitHub: {
+    href: 'https://github.com/CLooker/react-redux-readable',
+    imgExtension: 'jpg'
+  },
+  Résumé: {
+    href:
+      'https://docs.google.com/document/d/14Ghmep9aoV91R755AFMAyPtIO5Hfh_5p4aTDJG6vpug/edit?usp=sharing',
+    imgExtension: 'png'
+  },
+  Portfolio: {
+    href: 'https://clooker.github.io/portfolio.html',
+    imgExtension: 'jpg'
+  }
+};
 
 const Footer = () => (
-  <div className="footer">
-    <LinkedIn />
-    <GitHub />
-    <Resume />
-    <Portfolio />
+  <div className='footer'>
+    {Object.keys(footerItems).map(title => (
+      <a
+        key={title}
+        className={title.toLowerCase()}
+        title={title}
+        href={footerItems[title].href}
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <img
+          alt={title}
+          src={require(`../assets/${title.toLowerCase()}_logo.${
+            footerItems[title].imgExtension
+          }`)}
+        />
+      </a>
+    ))}
   </div>
 );
 
