c73b635d9bd7f3e8b7a1adc27a84db8c01d5c78a
CLooker <chad.looker@gmail.com>
2018-12-08 17:23:23 -0500

    add titles for sorting elements

--- src/components/SortPosts.js
+++ src/components/SortPosts.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
 const SortPosts = ({ sortByTimeStamp, sortByVoteScore, order }) => (
-  <div className="sort-posts">
+  <div className='sort-posts'>
     <button
       onClick={sortByTimeStamp}
       style={{ opacity: order === 'sortByTimeStamp' ? 1 : 0.5 }}
+      title='Sort by Newest'
     >
       Newest
     </button>
     <button
       onClick={sortByVoteScore}
       style={{ opacity: order === 'sortByVoteScore' ? 1 : 0.5 }}
+      title='Sort by Vote'
     >
       Best
     </button>
